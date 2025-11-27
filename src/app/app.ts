0c2cf608941b03c4814a9a395c70042bca7997ff
Carolina Dias <carolinatdias@live.com>
2025-11-27 15:02:50 -0300

    logos: import component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Logos } from './logos/logos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Logos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
