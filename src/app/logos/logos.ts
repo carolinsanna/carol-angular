39b6a682a2d0266cdc81831132545a40e2b114f6
Carolina Dias <carolinatdias@live.com>
2025-11-27 15:01:49 -0300

    logos: setup

--- src/app/logos/logos.ts
+++ src/app/logos/logos.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-logos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './logos.html',
   styleUrl: './logos.css',
 })
 export class Logos {
-
+  logos = [
+    'assets/logos/logo1.png',
+    'assets/logos/logo2.png',
+    'assets/logos/logo3.png',
+    'assets/logos/logo4.png',
+    'assets/logos/logo5.png'
+  ]
 }
