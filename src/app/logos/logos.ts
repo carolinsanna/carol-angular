c563a9aae9a1bc71aa97293b99e8891e5c968a37
Carolina Dias <carolinatdias@live.com>
2025-11-27 13:49:26 -0300

    feat: create logos component

---
+++ src/app/logos/logos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logos',
+  imports: [],
+  templateUrl: './logos.html',
+  styleUrl: './logos.css',
+})
+export class Logos {
+
+}
