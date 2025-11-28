d332eb372baa773375138f58d6f7f4720c25548b
Carolina Dias <carolinatdias@live.com>
2025-11-28 01:25:55 -0300

    fix: logo animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,19 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-    content: ['./src/**/*.{html,ts}'],
-    theme: {
-        extend: {
-            keyframes: {
-                logoLoop: {
-                    from: { transform: 'translateX(0)' },
-                    to: { transform: 'translateX(-50%)' }
-                }
-            },
-            animation: {
-                'logo-loop': 'logoLoop 20s linear infinite'
-            }
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      keyframes: {
+        logoLoop: {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-50%)' }
         }
-    },
-    plugins: []
+      },
+      animation: {
+        'logo-loop': 'logoLoop 20s linear infinite'
+      }
+    }
+  },
+  plugins: []
 } satisfies Config
