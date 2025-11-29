9ec0069b19ced7b9d7ab65716918ae323a4ead32
Carolina Dias <carolinatdias@live.com>
2025-11-29 19:55:02 -0300

    fix: card import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Logos } from './logos/logos';
+import { Card } from './card/card';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Logos],
+  imports: [RouterOutlet, Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
